bdfcb7b80ee0d41e257206a4a20bd2f75bc4f70c
Tyler Marriner <marriner.tyler@gmail.com>
2019-03-11 13:28:23 +1300

    fixed pc changes

--- WRAMPsys.srcs/sources_1/program_counter.v
+++ WRAMPsys.srcs/sources_1/program_counter.v
@@ -24,18 +24,20 @@ module program_counter (
 	assign read_value = read_enable ? {12'h000,pc_reg} : 32'hzzzzz;
 
 	always @(posedge clk) begin
-	if (!rst_n)
-		// We reset to the start of ROM 
-		// This is a virtual address that points to the rom hardware.
-		pc_reg <= 20'h80000; //WRAMPmon will be loaded here by the ROM module
-		
-		// If we want to skip WRAMPmon, we can just load a program straight into ram
-		//pc_reg <= 20'h00000; //FOR DEBUGGING ONLY DO NOT BUILD WITH THIS SETTING
-		
-	else if (increment)
-		pc_reg <= pc_reg + 1;
-	else if (write_enable)
-		pc_reg <= write_value[19:0];
-	end
+        if (!rst_n) begin
+            // We reset to the start of ROM 
+            // This is a virtual address that points to the rom hardware.
+            pc_reg <= 20'h80000; //WRAMPmon will be loaded here by the ROM module
+            
+            // If we want to skip WRAMPmon, we can just load a program straight into ram
+            //pc_reg <= 20'h00000; //FOR DEBUGGING ONLY DO NOT BUILD WITH THIS SETTING
+        end   
+        else if (increment) begin
+            pc_reg <= pc_reg + 1;
+        end
+        else if (write_enable) begin 
+            pc_reg <= write_value[19:0];
+        end
+    end
 
 endmodule
